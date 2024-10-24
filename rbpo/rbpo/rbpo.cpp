b61139f38fe3a533df26dff368409c619f70c341
nafanyyyy <94144717+nafanyyyy@users.noreply.github.com>
2024-10-24 23:27:01 +0300

    first

---
+++ rbpo/rbpo/rbpo.cpp
@@ -0,0 +1,198 @@
+﻿#include "framework.h"
+#include <shellapi.h>
+#include "rbpo.h"
+
+#define MAX_LOADSTRING 100
+
+#define ID_TRAY_APP_ICON 1001
+#define ID_TRAY_EXIT 1002
+#define WM_TRAYICON (WM_USER + 1)
+HINSTANCE  hInst;
+NOTIFYICONDATA nid;
+WCHAR szTitle[MAX_LOADSTRING];
+WCHAR szWindowClass[MAX_LOADSTRING];
+
+ATOM                RegisterMainWindowClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void AddTrayIcon(HWND);
+void RemoveTrayIcon();
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_RBPO, szWindowClass, MAX_LOADSTRING);
+    RegisterMainWindowClass(hInstance);
+
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_RBPO));
+
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+ATOM RegisterMainWindowClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_RBPO));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_RBPO);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance;
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        AddTrayIcon(hWnd);
+        break;
+
+    case WM_TRAYICON:
+        if (LOWORD(lParam) == WM_RBUTTONUP) {
+            // Правый клик на иконку в трее — показываем контекстное меню
+            POINT pt;
+            GetCursorPos(&pt);
+            HMENU hMenu = CreatePopupMenu();
+            InsertMenu(hMenu, 0, MF_BYPOSITION | MF_STRING, IDM_EXIT, L"Выход");
+            SetForegroundWindow(hWnd);
+            TrackPopupMenu(hMenu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, hWnd, NULL);
+            DestroyMenu(hMenu);
+        }
+        else if (LOWORD(lParam) == WM_LBUTTONDBLCLK) {
+            // Двойной щелчок по иконке трея — показать окно
+            ShowWindow(hWnd, SW_SHOW);
+            SetForegroundWindow(hWnd);
+        }
+        break;
+
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case IDM_ABOUT:
+            // Открытие диалога "О программе"
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+
+        case IDM_EXIT:
+            // Обработка выхода через контекстное меню
+            int result = MessageBox(hWnd, L"Вы уверены, что хотите закрыть приложение?", L"Подтверждение", MB_YESNO | MB_ICONQUESTION);
+            if (result == IDYES) {
+                RemoveTrayIcon();
+                PostQuitMessage(0);
+            }
+            break;
+        }
+        break;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // Рисование окна, если требуется
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
+    case WM_CLOSE:
+        // Скрываем окно вместо его закрытия
+        ShowWindow(hWnd, SW_HIDE);
+        break;
+
+    case WM_DESTROY:
+        // Убираем иконку из трея при завершении программы
+        RemoveTrayIcon();
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+void AddTrayIcon(HWND hWnd) {
+    nid.cbSize = sizeof(NOTIFYICONDATA);
+    nid.hWnd = hWnd;
+    nid.uID = ID_TRAY_APP_ICON;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uCallbackMessage = WM_TRAYICON; 
+    nid.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_RBPO));
+    wcscpy_s(nid.szTip, szTitle);
+
+    Shell_NotifyIcon(NIM_ADD, &nid);
+}
+
+void RemoveTrayIcon() {
+    Shell_NotifyIcon(NIM_DELETE, &nid);
+}
